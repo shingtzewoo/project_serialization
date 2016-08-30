9d1387fdae0b032ec1e64554d289f38cb3cfdf44
shingtzewoo@gmail.com <shingtzewoo@gmail.com>
2016-08-30 13:46:52 -0700

    add pseudo code for zip codes

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -4,5 +4,11 @@ puts "EventManager initialized."
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
   name = row[:first_name]
-  puts name
+  zipcode = row[:zipcode]
+
+  # if the zip code is exactly five digits, assume that it is ok
+  # if the zip code is more than 5 digits, truncate it to the first 5 digits
+  # if the zip code is less than 5 digits, add zeros to the front until it becomes five digits
+
+  puts "#{name} #{zipcode}"
 end
