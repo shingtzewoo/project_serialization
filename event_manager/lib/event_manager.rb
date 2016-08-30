0f3b8fb660e1805239bce51e14eec7f04abdd758
shingtzewoo@gmail.com <shingtzewoo@gmail.com>
2016-08-30 16:59:03 -0700

    refactor code, legislator_by_zipcode is neater due to erb file

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,25 +1,23 @@
-require "csv"
-require "sunlight/congress"
-template_letter = File.read("form_letter.html")
+require 'csv'
+require 'sunlight/congress'
+require 'erb'
 
 Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
 def clean_zipcode(zipcode)
-  zipcode.to_s.rjust(5, "0")[0..4]
+  zipcode.to_s.rjust(5,"0")[0..4]
 end
 
 def legislators_by_zipcode(zipcode)
-  legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
-
-  legislator_names = legislators.collect do |legislator|
-    "#{legislator.first_name} #{legislator.last_name}"
-  end
-
-  legislator_names.join(", ")
+  Sunlight::Congress::Legislator.by_zipcode(zipcode)
 end
 
 puts "EventManager initialized."
-contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+
+contents = CSV.open 'event_attendees.csv', headers: true, header_converters: :symbol
+
+template_letter = File.read "form_letter.erb"
+erb_template = ERB.new template_letter
 
 contents.each do |row|
   name = row[:first_name]
@@ -28,8 +26,6 @@ contents.each do |row|
 
   legislators = legislators_by_zipcode(zipcode)
 
-  personal_letter = template_letter.gsub('FIRST_NAME',name)
-  personal_letter = personal_letter.gsub('LEGISLATORS',legislators)
-
-  puts "#{name} #{zipcode} #{legislators} #{personal_letter}"
+  form_letter = erb_template.result(binding)
+  puts form_letter
 end
