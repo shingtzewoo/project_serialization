c65ca5686ab9af1e1f2fe822c2c4582b7f7721b6
shingtzewoo@gmail.com <shingtzewoo@gmail.com>
2016-08-29 17:10:42 -0700

    use Ruby's CSV library for parse instead

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -1,9 +1,8 @@
-puts "EventManager initialized!"
+require "csv"
+puts "EventManager initialized."
 
-lines = File.readlines "event_attendees.csv"
-lines.each_with_index do |line, index|
-  next if index == 0
-  columns = line.split(",")
-  name = columns[2]
+contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
+contents.each do |row|
+  name = row[:first_name]
   puts name
 end
