c183c59f45ea188a2162b49df31ccb6891ddc4fa
shingtzewoo@gmail.com <shingtzewoo@gmail.com>
2016-08-29 17:01:13 -0700

    add code to print out first names

--- event_manager/lib/event_manager.rb
+++ event_manager/lib/event_manager.rb
@@ -2,5 +2,7 @@ puts "EventManager initialized!"
 
 lines = File.readlines "event_attendees.csv"
 lines.each do |line|
-  puts line
+  columns = line.split(",")
+  name = columns[2]
+  puts name
 end
